887abaa5073876d6227d36f45b667784c679b5b9
Erick Pereira <ericksonxd_164@hotmail.com>
2025-06-18 12:33:08 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,20 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py makemigrations adminuser
+python manage.py makemigration cart
+python manage.py makemigrations home
+python manage.py makemigrations orders
+python manage.py makemigrations shop
+
+
+python manage.py migrate adminuser
+python manage.py migrate cart
+python manage.py migrate home
+python manage.py migrate orders
+python manage.py migrate shop
+
+
 # Apply database migrations
 python manage.py migrate
 
